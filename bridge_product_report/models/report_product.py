acc3328e4918c8afb848bf90007702ab753bd582
420Titania <kashira.naratama@student.sgu.ac.id>
2026-04-07 23:09:28 +0700

    Before using PNGs for labels

--- bridge_product_report/models/report_product.py
+++ bridge_product_report/models/report_product.py
@@ -8,10 +8,38 @@ class ProductReport(models.AbstractModel):
     _description = 'Product PDF Report'
 
     @api.model
+    def _get_status_summary(self, docs):
+        summary = {
+            'broken': 0,
+            'rusak': 0,
+            'dihibahkan': 0,
+            'tidak_ada': 0,
+        }
+
+        for product in docs:
+            status = (product.x_condition_status or '').strip().lower()
+            if status == 'broken':
+                summary['broken'] += 1
+            elif status == 'rusak':
+                summary['rusak'] += 1
+            elif status == 'dihibahkan':
+                summary['dihibahkan'] += 1
+            elif status == 'tidak ada':
+                summary['tidak_ada'] += 1
+
+        return summary
+
+    @api.model
     def _get_logo_data(self):
         """Return (base64_string, mime_type) for module logo."""
+        preferred_logo = get_module_resource(
+            'bridge_product_report', 'static', 'src', 'img', 'logo.png'
+        )
+        if preferred_logo:
+            with open(preferred_logo, 'rb') as logo_file:
+                return base64.b64encode(logo_file.read()).decode('ascii'), 'image/png'
+
         candidates = [
-            ('logo.png', 'image/png'),
             ('logo.jpeg', 'image/jpeg'),
             ('logo.jpg', 'image/jpeg'),
         ]
@@ -30,10 +58,16 @@ class ProductReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         docs = self.env['product.template'].browse(docids)
         logo_base64, logo_mime = self._get_logo_data()
+        web_base_url = (
+            self.env['ir.config_parameter'].sudo().get_param('web.base.url', '')
+        ).rstrip('/')
+        status_summary = self._get_status_summary(docs)
         return {
             'doc_ids': docids,
             'doc_model': 'product.template',
             'docs': docs,
             'logo_base64': logo_base64,
             'logo_mime': logo_mime,
+            'web_base_url': web_base_url,
+            'status_summary': status_summary,
         }
